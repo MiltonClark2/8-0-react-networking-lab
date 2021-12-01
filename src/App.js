fae717b5a74d0aaec7f7078881139e605d30b67a
Milton Clark <kingcoder247@gmail.com>
2021-11-30 19:39:33 -0500

    Change - convert App to class component to set state

--- src/App.js
+++ src/App.js
@@ -1,14 +1,26 @@
 import React from "react";
 import NavBar from "./Components/NavBar";
 import EmployeeList from "./Components/EmployeeList";
+import { Component } from "react";
 
-const App = () => {
-  return (
-    <>
-      <NavBar />
-      <EmployeeList />
-    </>
-  );
-};
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      
+    }
+  }
+  
+  render(){
+    return(
+      <div>
+        <NavBar />
+        <EmployeeList />
+      </div>
+    )
+  }
+}
+
 
 export default App;
